bae671dbc20109f793d2799819ec2941a8d8eff4
connoraleks <connoraleks@gmail.com>
2022-09-08 13:54:03 -0400

    feat-in-progress: dirent structure

---
+++ src/components/BasicSelect.jsx
@@ -0,0 +1,34 @@
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+const BasicSelect = ({label, values, val, setVal}) => {
+
+  const handleChange = (event) => {
+    setVal(event.target.value);
+  };
+  return (
+    <Box sx={{ minWidth: 120, maxWidth: 'fit-content' }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">{label}</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={val}
+          label={label}
+          onChange={handleChange}
+        >
+            {values.map((value, index) => {
+                return (
+                    <MenuItem value={index}>{value}</MenuItem>
+                )
+                })}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
+
+export default BasicSelect;
